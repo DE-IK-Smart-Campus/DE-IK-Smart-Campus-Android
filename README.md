e2e70587a41ebddfa8987613ddd8e04902e881fa
Balázs Bak <Balzs Bak>
2017-02-20 12:29:55 +0100

    initial android project

---
+++ README.md
@@ -0,0 +1,1 @@
+"# DE-IK-Smart-Campus-Android" 
---
+++ app/src/main/java/hu/unideb/smartcampus/SmartCampus.java
@@ -0,0 +1,13 @@
+package hu.unideb.smartcampus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmartCampus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smart_campus);
+    }
+}
