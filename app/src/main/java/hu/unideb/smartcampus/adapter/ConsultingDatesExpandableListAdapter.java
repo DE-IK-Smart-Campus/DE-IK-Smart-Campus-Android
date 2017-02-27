55d088383470318ad54ff029efd13973c677e51c
Erdei Krisztian <headswitcher@gmail.com>
2017-02-27 09:48:11 +0100

    ch work in progress

---
+++ app/src/main/java/hu/unideb/smartcampus/adapter/ConsultingDatesExpandableListAdapter.java
@@ -0,0 +1,104 @@
+package hu.unideb.smartcampus.adapter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import hu.unideb.smartcampus.R;
+import hu.unideb.smartcampus.adapter.consultingHours.dataObjects.Class;
+import hu.unideb.smartcampus.adapter.consultingHours.dataObjects.Teacher;
+
+/**
+ * Created by Headswitcher on 2017. 02. 24..
+ */
+
+public class ConsultingDatesExpandableListAdapter extends BaseExpandableListAdapter {
+
+    List<Teacher> teacherList;
+    Context context;
+
+    public ConsultingDatesExpandableListAdapter(Context context, List<Teacher> teacherList) {
+        this.teacherList = teacherList;
+        this.context = context;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return teacherList.size();
+    }
+
+    @Override
+    public int getChildrenCount(int classAt) {
+        return teacherList.get(classAt).getConsultingHours().getDateList().size();
+    }
+
+    @Override
+    public Object getGroup(int at) {
+        return teacherList.get(at);
+    }
+
+    @Override
+    public Object getChild(int teacherAt, int consultingHoursAt) {
+        return teacherList.get(teacherAt).getConsultingHours().getDateList().get(consultingHoursAt);
+    }
+
+    @Override
+    public long getGroupId(int teacherAt) {
+        return teacherAt;
+    }
+
+    @Override
+    public long getChildId(int teacherAt, int consultingHoursAt) {
+        return consultingHoursAt;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int teacherAt, boolean isExpanded, View view, ViewGroup parent) {
+        String headerTitle = teacherList.get(teacherAt).getName();
+        if (view == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(R.layout.list_teacher, null);
+        }
+        TextView teacherNameTextView = (TextView) view
+                .findViewById(R.id.teacherListItemId);
+        teacherNameTextView.setTypeface(null, Typeface.BOLD);
+        teacherNameTextView.setText(headerTitle);
+
+        return view;
+
+    }
+
+    @Override
+    public View getChildView(int teacherAt, int consultingHoursAt, boolean isExpanded, View view, ViewGroup viewGroup) {
+        final String dateDisplayName = teacherList.get(teacherAt).getConsultingHours().getDateList().get(consultingHoursAt).toString();
+
+        if (view == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = infalInflater.inflate(R.layout.list_consultinghours, null);
+        }
+
+        TextView consultingHoursTextView = (TextView) view
+                .findViewById(R.id.consultingHoursListItemId);
+
+        consultingHoursTextView.setText(dateDisplayName);
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
+    }
+}
--- app/src/main/java/hu/unideb/smartcampus/consultinghours/ConsultingHours.java
+++ app/src/main/java/hu/unideb/smartcampus/consultinghours/ConsultingHours.java
@@ -3,15 +3,23 @@ package hu.unideb.smartcampus.consultinghours;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import hu.unideb.smartcampus.R;
+import hu.unideb.smartcampus.adapter.ConsultingDatesExpandableListAdapter;
 import hu.unideb.smartcampus.adapter.ConsultingHoursExpandableListAdapter;
 import hu.unideb.smartcampus.adapter.consultingHours.dataObjects.Class;
+import hu.unideb.smartcampus.adapter.consultingHours.dataObjects.ConsultingHoursObject;
 import hu.unideb.smartcampus.adapter.consultingHours.dataObjects.Teacher;
 
 /**
@@ -26,25 +34,46 @@ public class ConsultingHours extends AppCompatActivity {
         setContentView(R.layout.activity_consulting_hours);
 
         //Mocked data , we will need some functions here
-        ExpandableListView ClassList = (ExpandableListView) findViewById(R.id.consulting_hours_ExpandableListView);
+
+        //Refactor inc for dates
+        final ExpandableListView ClassList = (ExpandableListView) findViewById(R.id.consulting_hours_ExpandableListView);
         List<Teacher> teacherList = new ArrayList<>();
         List<Teacher> teacherList1 = new ArrayList<>();
         List<Teacher> teacherList2 = new ArrayList<>();
-        teacherList.add(new Teacher("Várterész Magdolna", null));
-        teacherList.add(new Teacher("Kádek Tamás", null));
-        teacherList.add(new Teacher("Dr. Horváth Géza", null));
+        Date date = new Date();
+        List<Date> dateList = Arrays.asList(date);
+        ConsultingHoursObject consultingHours = new ConsultingHoursObject(dateList);
+
+        teacherList.add(new Teacher("Várterész Magdolna", consultingHours));
+        teacherList.add(new Teacher("Kádek Tamás", consultingHours));
+        teacherList.add(new Teacher("Dr. Horváth Géza", consultingHours));
         Class mestint = new Class("A mesterséges intelligencia alapjai", teacherList);
-        teacherList1.add(new Teacher("Dr. Gál Zoltán", null));
-        teacherList1.add(new Teacher("Dr. Szilágyi Szabolcs", null));
-        teacherList1.add(new Teacher("Vas Ádám", null));
+        teacherList1.add(new Teacher("Dr. Gál Zoltán", consultingHours));
+        teacherList1.add(new Teacher("Dr. Szilágyi Szabolcs", consultingHours));
+        teacherList1.add(new Teacher("Vas Ádám", consultingHours));
         Class halo = new Class("Hálózati architektúrák és protokollok", teacherList1);
-        teacherList2.add(new Teacher("Dr. Jeszenszky Péter", null));
-        Class internet = new Class("Az internet eszközei és szolgáltatásai", teacherList2);
+        teacherList2.add(new Teacher("Dr. Jeszenszky Péter", consultingHours));
+        Class internet = new Class("Az internet eszközei és filtikai", teacherList2);
         List<Class> classList = new ArrayList<>();
         classList.add(mestint);
         classList.add(halo);
         classList.add(internet);
-        ExpandableListAdapter expandableListAdapter = new ConsultingHoursExpandableListAdapter(getApplicationContext(), classList);
-        ClassList.setAdapter(expandableListAdapter);
+        final ExpandableListAdapter ClassChildTeacherListAdapter = new ConsultingHoursExpandableListAdapter(getApplicationContext(), classList);
+        ClassList.setAdapter(ClassChildTeacherListAdapter);
+
+        ClassList.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int classAt, int childPosition, long id) {
+                Log.v("Itt","ez");
+
+               Class parentClass = (Class) parent.getExpandableListAdapter().getGroup(classAt);
+                //refactor
+                ExpandableListAdapter teacherChildConsultingHoursListAdapter =
+                        new ConsultingDatesExpandableListAdapter(getApplicationContext(),parentClass.getTeacherList());
+                ClassList.setAdapter(teacherChildConsultingHoursListAdapter);
+                return true;
+            }
+        });
+
     }
 }
