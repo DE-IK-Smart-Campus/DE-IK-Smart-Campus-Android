1d5f9d3deb1f27794b8cc2e3fea0a930c54b7702
Bak Balazs <bak.balazs95@gmail.com>
2017-04-08 03:52:22 +0200

    connection status ok

--- app/src/main/java/hu/unideb/smartcampus/main/activity/login/auth/BasicAuth.java
+++ app/src/main/java/hu/unideb/smartcampus/main/activity/login/auth/BasicAuth.java
@@ -41,47 +41,9 @@ public class BasicAuth extends AsyncTask<ActualUserInfo, Long, ActualUserInfo> {
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final int HTTP_OK_STATUS = 200;
 
-    //Unsafe! TODO
-    private static OkHttpClient getUnsafeOkHttpClient() {
-        try {
-            final TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-                        @Override
-                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                        }
-
-                        @Override
-                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-                        }
-
-                        @Override
-                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                            return null;
-                        }
-                    }
-            };
-            final SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-            OkHttpClient okHttpClient = new OkHttpClient();
-            okHttpClient.setSslSocketFactory(sslSocketFactory);
-            okHttpClient.setHostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                    // TODO !!!!!!!!!!
-                }
-            });
-
-            return okHttpClient;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     @Override
     protected ActualUserInfo doInBackground(ActualUserInfo... params) {
-        OkHttpClient httpClient = getUnsafeOkHttpClient();
+        OkHttpClient httpClient = new OkHttpClient();
         ObjectMapper objectMapper = new ObjectMapper();
 
         ActualUserInfo actualUserInfo = params[0];
