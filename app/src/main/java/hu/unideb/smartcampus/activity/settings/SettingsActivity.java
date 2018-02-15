3b83827b218072f77abf9848ca077fb604968084
Bak Balázs <bak.balazs95@gmail.com>
2018-02-15 13:12:19 +0100

    colorpreference

--- app/src/main/java/hu/unideb/smartcampus/activity/settings/SettingsActivity.java
+++ app/src/main/java/hu/unideb/smartcampus/activity/settings/SettingsActivity.java
@@ -5,15 +5,22 @@ import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 
+import com.jaredrummler.android.colorpicker.ColorPreference;
+
 import butterknife.ButterKnife;
 import hu.unideb.smartcampus.R;
 import hu.unideb.smartcampus.activity.base.BaseActivity;
+
+import static hu.unideb.smartcampus.container.Container.TIMETABLE_EVENT_COLOR_CHOOSER_KEY;
 
 public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+
+        final ColorPreference colorPreference = (ColorPreference) findPreference(TIMETABLE_EVENT_COLOR_CHOOSER_KEY);
+
     }
 
     //    @Override
