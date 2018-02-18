d1496873f70c35ae540302251dd1a19e341a67e6
Bak Balázs <bak.balazs95@gmail.com>
2018-02-19 00:14:43 +0100

    custom event activity layout change

--- app/src/main/java/hu/unideb/smartcampus/activity/calendar/NewCustomEventActivity.java
+++ app/src/main/java/hu/unideb/smartcampus/activity/calendar/NewCustomEventActivity.java
@@ -21,7 +21,7 @@ public class NewCustomEventActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_event_activity);
+        setContentView(R.layout.activity_new_event);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_acivity);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
