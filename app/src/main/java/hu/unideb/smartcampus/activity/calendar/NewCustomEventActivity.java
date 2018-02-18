aa7c04e57db656d454f3501e26ef21a8b9cd4536
Bak Balázs <bak.balazs95@gmail.com>
2018-02-19 00:31:56 +0100

    new event activty show selected date

--- app/src/main/java/hu/unideb/smartcampus/activity/calendar/NewCustomEventActivity.java
+++ app/src/main/java/hu/unideb/smartcampus/activity/calendar/NewCustomEventActivity.java
@@ -5,8 +5,14 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import hu.unideb.smartcampus.R;
 import hu.unideb.smartcampus.activity.base.BaseActivity;
@@ -17,6 +23,33 @@ public class NewCustomEventActivity extends BaseActivity {
 
     // yyyy.mm.nap  magyar
     // nap.hónap.év angol
+
+    @BindView(R.id.event_name_edit_text)
+    EditText eventNameEditText;
+
+    @BindView(R.id.event_description_edit_text)
+    EditText eventDescriptionEditText;
+
+    @BindView(R.id.event_place_edit_text)
+    EditText eventPlaceEditText;
+
+    @BindView(R.id.event_start_date_edit_text)
+    EditText eventStartDateEditText;
+
+    @BindView(R.id.event_start_time_edit_text)
+    EditText eventStartTimeEditText;
+
+    @BindView(R.id.event_end_date_edit_text)
+    EditText eventEndDateEditText;
+
+    @BindView(R.id.event_end_time_edit_text)
+    EditText eventEndTimeEditText;
+
+    @BindView(R.id.event_repeat_edit_text)
+    EditText eventRepeatEditText;
+
+    @BindView(R.id.event_notification_edit_text)
+    EditText eventNotificationEditText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,7 +62,15 @@ public class NewCustomEventActivity extends BaseActivity {
         getSupportActionBar().setTitle(R.string.new_event_text);
         ButterKnife.bind(this);
 
-        getIntent().getExtras().getLong(SELECTED_DATE_LONG);
+        Long startDate = getIntent().getExtras().getLong(SELECTED_DATE_LONG);
+
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy.MMM dd.,EEE", Locale.getDefault());
+
+        String selectedDateCalendar = dateFormatter.format(startDate);
+
+        eventStartDateEditText.setText(selectedDateCalendar);
+
+
     }
 
     @Override
