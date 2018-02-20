c141b99f596eb61064ce16660cd6528f3dcd1f57
Bak Balázs <bak.balazs95@gmail.com>
2018-02-20 16:15:35 +0100

    add new date and time picker and start date and start time picker work

--- app/src/main/java/hu/unideb/smartcampus/activity/calendar/NewCustomEventActivity.java
+++ app/src/main/java/hu/unideb/smartcampus/activity/calendar/NewCustomEventActivity.java
@@ -3,30 +3,34 @@ package hu.unideb.smartcampus.activity.calendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.List;
+import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import hu.unideb.smartcampus.R;
 import hu.unideb.smartcampus.activity.base.BaseActivity;
-import hu.unideb.smartcampus.pojo.calendar.TimetableEvent;
 
 import static hu.unideb.smartcampus.container.Container.EVENT_DATE_FORMAT_PATTERN_EN;
 import static hu.unideb.smartcampus.container.Container.EVENT_DATE_FORMAT_PATTERN_HU;
 import static hu.unideb.smartcampus.container.Container.EVENT_TIME_FORMAT_PATTERN;
 import static hu.unideb.smartcampus.container.Container.SELECTED_DATE_LONG;
 
-public class NewCustomEventActivity extends BaseActivity {
+public class NewCustomEventActivity extends BaseActivity implements TimePickerDialog.OnTimeSetListener , DatePickerDialog.OnDateSetListener {
 
     @BindView(R.id.event_name_edit_text)
     EditText eventNameEditText;
@@ -55,6 +59,11 @@ public class NewCustomEventActivity extends BaseActivity {
     @BindView(R.id.event_notification_edit_text)
     EditText eventNotificationEditText;
 
+    TimePickerDialog tpd = new TimePickerDialog();
+    Long startDate;
+
+    com.wdullaer.materialdatetimepicker.date.DatePickerDialog dpd = new com.wdullaer.materialdatetimepicker.date.DatePickerDialog();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,9 +76,60 @@ public class NewCustomEventActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
-        Long startDate = getIntent().getExtras().getLong(SELECTED_DATE_LONG);
+        startDate = getIntent().getExtras().getLong(SELECTED_DATE_LONG);
 
         setDefaultDateAndTime(startDate);
+
+
+
+
+    }
+
+    @Override
+    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+        String hourString = hourOfDay < 10 ? "0"+hourOfDay : ""+hourOfDay;
+        String minuteString = minute < 10 ? "0"+minute : ""+minute;
+        String secondString = second < 10 ? "0"+second : ""+second;
+        SimpleDateFormat timeFormatter1 = new SimpleDateFormat(EVENT_TIME_FORMAT_PATTERN, Locale.getDefault());
+
+        Calendar  v = Calendar.getInstance();
+        v.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        v.set(Calendar.MINUTE, minute);
+        v.set(Calendar.SECOND,0);
+        Log.e("asd", timeFormatter1.format(new Date(v.getTimeInMillis())));
+        String time = hourString+":"+minuteString;
+        Log.e("ad", time);
+        eventStartTimeEditText.setText(time);
+    }
+
+
+    @OnClick(R.id.event_start_date_edit_text)
+    public void t1(){
+        Calendar now = Calendar.getInstance();
+        now.setTimeInMillis(startDate);
+
+        dpd = DatePickerDialog.newInstance(
+                this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+
+        dpd.setAccentColor(getResources().getColor(R.color.calendar_blue));
+        dpd.show(getFragmentManager(), "Timepickasdsaerdialog");
+
+
+    }
+
+    @OnClick(R.id.event_start_time_edit_text)
+    public void t(){
+        Calendar now = Calendar.getInstance();
+        now.setTimeInMillis(startDate);
+
+        tpd.setAccentColor(getResources().getColor(R.color.calendar_blue));
+        tpd.show(getFragmentManager(), "Timepickerdialog");
+
+
     }
 
     private void setDefaultDateAndTime(Long selectedDate) {
@@ -104,6 +164,13 @@ public class NewCustomEventActivity extends BaseActivity {
         startTimeCalendar.setTimeZone(TimeZone.getDefault());
         roundingThirtyMinutes(startTimeCalendar);
 
+        tpd.initialize(this,
+                startTimeCalendar.get(Calendar.HOUR_OF_DAY),
+                startTimeCalendar.get(Calendar.MINUTE),
+                startTimeCalendar.get(Calendar.SECOND),
+                true
+        );
+
         Calendar endTimeCalendar = Calendar.getInstance();
         endTimeCalendar.set(Calendar.SECOND, 0);
         endTimeCalendar.set(Calendar.MILLISECOND, 0);
@@ -123,6 +190,11 @@ public class NewCustomEventActivity extends BaseActivity {
         eventEndDateEditText.setText(formatEndDate);
         eventEndTimeEditText.setText(formatEndTime);
     }
+
+//    @OnClick(R.id.event_start_date_edit_text)
+//    public void eventStartDate() {
+//        Log.e("asd", "Lenyomva");
+//    }
 
     private void roundingThirtyMinutes(Calendar calendar) {
         int rounding = calendar.get(Calendar.MINUTE) % 30;
@@ -150,5 +222,28 @@ public class NewCustomEventActivity extends BaseActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, monthOfYear);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        SimpleDateFormat dateFormatter;
+
+        Locale defaultLocale = Locale.getDefault();
+        Locale hunLocale = new Locale("hu", "hu");
+
+        if (defaultLocale.getLanguage().equals(hunLocale.getLanguage())) {
+            dateFormatter = new SimpleDateFormat(EVENT_DATE_FORMAT_PATTERN_HU, defaultLocale);
+        } else {
+            dateFormatter = new SimpleDateFormat(EVENT_DATE_FORMAT_PATTERN_EN, defaultLocale);
+        }
+
+        eventStartDateEditText.setText(dateFormatter.format(new Date(c.getTimeInMillis())));
+
+
     }
 }
